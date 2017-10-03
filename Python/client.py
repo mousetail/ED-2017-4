5eb601f1622f12ec9d77c4b991dd50116c2499df
Maurits van Riezen <MauritsVanRiezen@gmail.com>
2017-10-03 14:59:14 +0200

    length prefix

--- Python/client.py
+++ Python/client.py
@@ -6,17 +6,29 @@ try:
 
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serversocket.connect((ip, 1024))
+	
+	length=""
+	lengthGot=False
 	#serversocket.send('hello')
 	print ("opening file...")
 	f = open("f.jpg", "w")
 	bytes = 0
 	while True:
 		t=serversocket.recv(1)
-		bytes+=len(t)
-		if (bytes%1000==0):
-			print bytes, "bytes read"
+		if lengthGot:
+			bytes+=len(t)
+			if (bytes%1000==0):
+				print bytes, "bytes read"
 		
-		f.write(t)
+			f.write(t)
+			if bytes == length:
+				break
+		else:
+			if t==';':
+				length = int(length)
+				lengthGot = True
+			else:
+				length += t
 except:
 	traceback.print_exception(*sys.exc_info())
 	raw_input()
